93e76f9131a3182ff4959a0bb93721526eb9fc3a
travisprol0 <travisjohnprol@gmail.com>
2021-08-23 14:04:03 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Udemy_Algos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hey");
+        }
+    }
+}
